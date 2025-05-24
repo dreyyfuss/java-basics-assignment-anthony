8eb0a1d3241c46175ff9451f1d29c24c89198cf9
Anthony Alikah <a.o.alikah@gmail.com>
2025-05-24 19:22:02 +0100

    Solve count character frequency using hashmap

--- CountCharacterFrequency.java
+++ CountCharacterFrequency.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
 public class CountCharacterFrequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Character, Integer> counts = new HashMap<>();
+
+        System.out.println("Enter a string:");
+        String input = scanner.nextLine();
+        for (Character c: input.toCharArray()) {
+            if (counts.containsKey(c)) {
+                Integer i = counts.put(c, counts.get(c) + 1);
+            }
+            else {
+                counts.put(c, 1);
+            }
+        }
+
+        System.out.println("The frequencies of characters are: "+counts);
+    }
 }
